102b9ed8920089466cb85189f556dbff2fff3f19
VikaBilyk <128689260+VikaBilyk@users.noreply.github.com>
2025-10-15 00:18:41 +0300

    connect to db tables under db context

---
+++ auth/auth/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Users> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Users>(entity =>
+        {
+            entity.ToTable("users"); 
+            entity.HasKey(u => u.Id);
+            entity.Property(u => u.Email).IsRequired();
+            entity.Property(u => u.Password).IsRequired();
+            entity.Property(u=>u.CreatedAt).IsRequired();
+        });
+    }
+}
