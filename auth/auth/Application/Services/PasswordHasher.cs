7d1b1ba4433f92cc7c83170f584b8651564c7291
VikaBilyk <128689260+VikaBilyk@users.noreply.github.com>
2025-10-20 02:50:51 +0300

    add hasher for password

---
+++ auth/auth/Application/Services/PasswordHasher.cs
@@ -0,0 +1,14 @@
+namespace Application;
+
+public class PasswordHasher
+{
+    public string HashPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public bool VerifyHashedPassword(string hashedPassword, string password)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+    }
+}
